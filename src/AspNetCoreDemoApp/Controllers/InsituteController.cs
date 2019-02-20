217c6c9882e4dfeb726783c02bbbf319bc9505a5
Sofiya Dublenich <sofiya.dubl@gmail.com>
2019-02-20 21:36:22 +0200

    Added Institue controller.

--- src/AspNetCoreDemoApp/Controllers/InsituteController.cs
+++ src/AspNetCoreDemoApp/Controllers/InsituteController.cs
@@ -1,11 +1,34 @@
-﻿using System;
+using AspNetCoreDemoApp.Common;
+using AspNetCoreDemoApp.Model;
+using AspNetCoreDemoApp.Validators;
+using Microsoft.AspNetCore.Mvc;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace AspNetCoreDemoApp.Controllers
 {
-    public class InsituteController
+    [Route("api/Institute")]
+    public class InsituteController : ControllerBase
     {
+        private readonly Context _context = new Context();
+
+        // POST: api/Institute/Add
+        [HttpPost]
+        [Route("Register")]
+        public ActionResult<Institute> AddInstitute([FromBody]Institute institute)
+        {
+            var institutes = _context.Institute;
+
+            if (InstituteValidator.IsInstituteExists(institute))
+            {
+                return BadRequest(ErrorHandler.GenerateError(1201, "Institute already exists."));
+            }
+
+            institutes.Add(institute);
+            _context.SaveChanges();
+            return institute;
+        }
     }
 }
