efabdcfc80c3c91872c437134ccf8b30f2f47218
Sofiya Dublenich <sofiya.dubl@gmail.com>
2019-02-21 10:52:21 +0200

    Deleted redundant method.

--- src/AspNetCoreDemoApp/Filters/UnhandledExceptionFilter.cs
+++ src/AspNetCoreDemoApp/Filters/UnhandledExceptionFilter.cs
@@ -11,14 +11,6 @@ namespace AspNetCoreDemoApp.Filters
 
     public class UnhandledExceptionFilter : ActionFilterAttribute, IExceptionFilter
     {
-        public override void OnActionExecuting(ActionExecutingContext context)
-        {
-            if (!AuthenticationManager.IsAuthenticated(context.HttpContext.Request))
-            {
-                context.Result = new BadRequestObjectResult(AuthenticationManager.UnauthorizedError());
-            }
-        }
-
         public void OnException(ExceptionContext context)
         {
             context.Result = new BadRequestObjectResult(ErrorHandler.GenerateError(999, ErrorHandler.MessageFromException(context.Exception)));
